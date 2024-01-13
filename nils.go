8d6e7682dbed1b1d227de47f81908df9b2e87fb0
Esteban Camargo <estebmaister@gmail.com>
2024-01-13 12:36:21 -0500

    polishing the details in nils file

--- nils.go
+++ nils.go
@@ -5,32 +5,42 @@ import (
 	"reflect"
 )
 
-func nilDiffs() {
+func NilDiffs() {
 	fmt.Println(
 		"Difference between a normal 'nil'",
-		"and an empty interface",
+		"and an empty interface:\n ",
 	)
-	AdultNil := adult(17)
+	AdultNil := adult(17) // Returns nil
+	// AdultNil.randomMethod() // Panic nil pointer
+
 	AdultInterface := adult(23)
+	AdultInterface.randomMethod()
+
 	fmt.Println(
-		"Using reflect type, 'pure nil':",
+		"Using reflect type,\n 'pure nil':",
 		reflect.TypeOf(AdultNil),
 		"vs 'interface nil':",
-		reflect.TypeOf(AdultInterface),
-	)
-	fmt.Println("Printing both values:",
+		reflect.TypeOf(&AdultInterface),
+	) // different values
+
+	fmt.Println("pure nil == interface nil?\n",
+		AdultNil == AdultInterface,
+	) // false
+
+	fmt.Println("Printing both values:\n",
 		AdultNil,
 		AdultInterface,
-	)
+	) // same value printed
 }
 
 type random interface {
 	randomMethod()
 }
 
-type ir struct{}
+type implRandom struct{}
 
-func (i ir) randomMethod() {
+// Needs to be assign to pointer to be called from interface
+func (i *implRandom) randomMethod() {
 	// Do nothing
 }
 
@@ -38,6 +48,6 @@ func adult(n int) random {
 	if n < 18 {
 		return nil
 	}
-	var a *ir = nil
+	var a *implRandom = nil
 	return a
 }
